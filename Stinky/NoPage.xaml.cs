013563a9d745f12497ddad3caee4d73053b3235a
Ahri <ahripyx@gmail.com>
2025-03-31 01:09:34 -0400

    More audios, added images to yes and no page, and fixed nav bar showing when switching pages

--- Stinky/NoPage.xaml.cs
+++ Stinky/NoPage.xaml.cs
@@ -5,5 +5,6 @@ public partial class NoPage : ContentPage
 	public NoPage()
 	{
 		InitializeComponent();
-	}
+        NavigationPage.SetHasNavigationBar(this, false);
+    }
 }
--- Stinky/YesPage.xaml.cs
+++ Stinky/YesPage.xaml.cs
@@ -5,5 +5,6 @@ public partial class YesPage : ContentPage
 	public YesPage()
 	{
 		InitializeComponent();
-	}
+        NavigationPage.SetHasNavigationBar(this, false);
+    }
 }

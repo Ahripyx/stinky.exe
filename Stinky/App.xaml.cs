1830db8ad69085c3e109b981f4830d21cdb5a6cd
Ahri <ahripyx@gmail.com>
2025-03-30 23:53:31 -0400

    Add project files.

---
+++ Stinky/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace Stinky
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        int wndWidth = 800;
+        int wndHeight = 600;
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = new Window(new MainPage())
+            {
+                Width = wndWidth,
+                Height = wndHeight,
+                MaximumHeight = wndHeight,
+                MaximumWidth = wndWidth,
+                MinimumHeight = wndHeight,
+                MinimumWidth = wndWidth,
+            };
+
+            return window;
+        }
+    }
+}
---
+++ Stinky/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿namespace Stinky
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            ShowDialog();
+        }
+
+        private void ShowDialog()
+        {
+            dialogOverlay.IsVisible = true;
+            dialog.IsVisible = true;
+        }
+
+        private void HideDialog()
+        {
+            dialogOverlay.IsVisible = false;
+            dialog.IsVisible = false;
+        }
+
+
+        private void OnYesButtonClicked(object sender, EventArgs e)
+        {
+            HideDialog();
+            // Add your logic for the "Yes" button here
+        }
+
+        private void OnNoButtonClicked(object sender, EventArgs e)
+        {
+            HideDialog();
+            // Add your logic for the "No" button here
+        }
+    }
+
+}
